b96d3b036945963706ad9a71c4767ed3b8e2b92a
Rodrigo Agerri <agerri.rodrigo@gmail.com>
2013-03-13 17:14:47 +0000

    integration mods

--- lib/EHU-ner-lite_EN_kernel.rb
+++ lib/EHU-ner-lite_EN_kernel.rb
@@ -8,7 +8,7 @@ module Opener
 
       		class Configuration
         		CORE_DIR    = File.expand_path("../core", File.dirname(__FILE__))
-        		KERNEL_CORE = CORE_DIR+'/target/nerc-en-1.0.jar'
+        		KERNEL_CORE = CORE_DIR+'/ehu-opennlp-nerc-en-1.0.jar'
       		end
 
     	  end
