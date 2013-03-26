980af9d6c03527b910220d4361208b94357c44c3
sb-olr <sb-olr@opener>
2013-03-26 18:32:00 +0100

    changed command in lib file for test option

--- lib/EHU-ner-lite_EN_kernel.rb
+++ lib/EHU-ner-lite_EN_kernel.rb
@@ -13,16 +13,14 @@ module Opener
             def initialize
               core_dir    = File.expand_path("../core", File.dirname(__FILE__))
 
-              @kernel      = core_dir+'/ehu-opennlp-nerc-en-1.0.jar'
-              @lib         = core_dir+'/lib/'
+              @kernel      = core_dir+'/ehu-opennlp-nerc-en-1.0.jar -l en'
             end
 
             def command(opts={})
               arguments = opts[:arguments] || []
-              arguments << "-n" if opts[:test]
-#`cat #{@input} | java -jar #{KERNEL_CORE} -stdin > #{@output}`
-              #"java -jar #{kernel} #{opts[:input]} #{arguments.join(' ')}"
-              "cat #{opts[:input]} | java -jar #{kernel} -stdin #{arguments.join(' ')}"
+              arguments << "-t" if opts[:test]
+              
+		"cat #{opts[:input]} | java -jar #{kernel} #{arguments.join(' ')}"
 
             end
 
