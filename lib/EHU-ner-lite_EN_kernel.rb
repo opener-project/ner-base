a1c53cb2f6460eb0b58741299654378d75e9921d
Rodrigo Agerri <agerri.rodrigo@gmail.com>
2013-04-23 12:40:50 +0200

    fixtures updated

--- lib/EHU-ner-lite_EN_kernel.rb
+++ lib/EHU-ner-lite_EN_kernel.rb
@@ -5,7 +5,7 @@ module Opener
     module EHU
       module NER
         module Lite
-          class EN 
+          class EN
             VERSION = "0.0.2"
 
             attr_reader :kernel, :lib
@@ -13,13 +13,13 @@ module Opener
             def initialize
               core_dir    = File.expand_path("../core", File.dirname(__FILE__))
 
-              @kernel      = core_dir+'/ehu-opennlp-nerc-en-1.0.jar'
+              @kernel      = core_dir+'/ehu-nerc-1.0.jar'
             end
 
             def command(opts={})
               arguments = opts[:arguments] || []
               arguments << "-t" if opts[:test]
-              
+
 		"cat #{opts[:input]} | java -jar #{kernel} -l en #{arguments.join(' ')}"
 
             end
